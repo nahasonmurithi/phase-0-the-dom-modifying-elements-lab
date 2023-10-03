be54266266a70df551c613281465e8e9b61260c1
Nahason <nahason.murithi@student.moringaschool.com>
2023-10-03 21:41:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code here!
+main.remove();
+
+const element = document.createElement("h1");
+h1.setAttribute("id", "victory");
+
+h1.innerText = "YOUR-NAME is the champion"
+
+
+/*
+const element = document.createElement("div");
+document.body.appendChild(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+main.style.height = "300px";
+main.style.backgroundColor = "#27647B";
+main.style.fontSize = "24px";
+main.style.marginLeft = "30px";
+main.style.lineHeight = 2;
+
+*/
